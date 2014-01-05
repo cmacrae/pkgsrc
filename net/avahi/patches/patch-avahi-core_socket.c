34d26f085ee07cee06a2674c3b4677053b04717e
yrmt <youri.mout@gmail.com>
2014-01-05 15:06:23 +0100

    fixed avahi

--- net/avahi/patches/patch-avahi-core_socket.c
+++ net/avahi/patches/patch-avahi-core_socket.c
@@ -1,7 +1,4 @@
-$NetBSD: patch-avahi-core_socket.c,v 1.1 2013/07/05 16:28:43 ryoon Exp $
-
-* Fix build on NetBSD 6.99.23.
-  From martin@'s post on tech-pkg@.
+$NetBSD$
 
 --- avahi-core/socket.c.orig	2011-04-25 00:12:18.000000000 +0000
 +++ avahi-core/socket.c
