80bc6962bff4a872ae1bb34b5fc28010f8368727
yrmt <youri.mout@gmail.com>
2014-01-05 15:01:24 +0100

    removed incompatible ports

--- net/avahi/patches/patch-avahi-core_socket.c
+++ net/avahi/patches/patch-avahi-core_socket.c
@@ -5,15 +5,12 @@ $NetBSD: patch-avahi-core_socket.c,v 1.1 2013/07/05 16:28:43 ryoon Exp $
 
 --- avahi-core/socket.c.orig	2011-04-25 00:12:18.000000000 +0000
 +++ avahi-core/socket.c
-@@ -528,7 +528,11 @@ int avahi_send_dns_packet_ipv4(
-             pkti->ipi_ifindex = interface;
- 
-         if (src_address)
-+#ifdef __linux__
-             pkti->ipi_spec_dst.s_addr = src_address->address;
-+#else
-+            pkti->ipi_addr.s_addr = src_address->address;
-+#endif
-     }
- #elif defined(IP_MULTICAST_IF)
-     if (src_address) {
+@@ -16,7 +16,7 @@
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+   USA.
+ ***/
+-
++#define __APPLE_USE_RFC_2292
+ #ifdef HAVE_CONFIG_H
+ #include <config.h>
+ #endif
